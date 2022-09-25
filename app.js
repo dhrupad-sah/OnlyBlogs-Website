aea959c1d6659fa7343622a9e9e1a41f1ee393f6
Dhrupad <thedhrupad@gmail.com>
2022-09-26 00:18:27 +0530

    mongo changes

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const e = require("express");
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const blogSchema = new mongoose.Schema({
+  blogTitle: String,
+  blogContent: String,
+});
+
+const blogModel = new mongoose.model("blogModel", blogSchema);
 
 const homeStartingContent = "Welcome to my daily Blog. Create a beautiful blog that fits your style. Choose from a selection of easy-to-use templates – all with flexible layouts and hundreds of background images – or design something new.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -46,12 +56,18 @@ app.get("/compose", (req,res)=>
 
 app.post("/compose", (req,res)=>
 {
+  const postM= new blogModel({
+    blogTitle: req.body.postTitle,
+    blogContent: req.body.postBody
+  });
+  
   const post= {
     title: req.body.postTitle,
     content: req.body.postBody
   };
 
-  posts.push(post);
+  postM.save();
+  posts.push(post)
 
 res.redirect("/posts");
 });
